e2af0d1f883fa979ea85b008e988f9ab9c1d01ab
Shahriar <mollahshahriar@outlook.com>
2019-12-18 17:29:08 -0500

    Update StudentDatabase Final.sql

--- StudentDatabase Final.sql
+++ StudentDatabase Final.sql
@@ -53,3 +53,30 @@ CREATE TABLE StudentDatabase.Scholarship(
 	StudentID int,
 	FOREIGN KEY (StudentID)REFERENCES  StudentDatabase.Students(StudentID)
 );
+
+
+--CREATE INDEX
+
+CREATE INDEX ix_Students_FirstName
+ON StudentDatabase.Students(FirstName)
+GO
+
+CREATE INDEX ix_Course_CourseName
+ON StudentDatabase.Course(CourseName)
+GO
+
+CREATE INDEX ix_Scholarship_ScholarshipID
+ON StudentDatabase.Scholarship(ScholarshipID)
+GO
+
+--ADD CONSTRAINT DEFAULT
+
+ALTER TABLE StudentDatabase.Course
+ADD CONSTRAINT ConsCourseStart
+DEFAULT GETDATE() for CourseStart
+
+--ADD CONSTRAINT CHECK
+
+ALTER TABLE StudentDatabase.Students
+ADD CONSTRAINT CHK_PostCode CHECK (PostCode LIKE'[a-z][0-9][a-z][0-9][a-z][0-9]');
+GO
